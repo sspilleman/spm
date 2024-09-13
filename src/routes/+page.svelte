ab30074e5949afc7e3c9989a089cd0b619ab5990
Sander Spilleman <sander@spilleman.nl>
2024-09-13 22:46:25 +0200

    2024-09-13T22-46-25

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 </svelte:head>
 
 <section class="mt-24 flex flex-row gap-4 w-full justify-center">
-	{#if $ratecard.length > 0}
+	{#if $ratecard && $ratecard.length > 0}
 		<a
 			href="/ratecard"
 			class="button w-48 h-48 bg-gray-200 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 flex flex-col rounded-xl justify-evenly"
@@ -21,7 +21,7 @@
 			<div class="flex flex-row justify-center dark:text-white mb-6">ratecard</div>
 		</a>
 	{/if}
-	{#if $usage.length > 0}
+	{#if $usage && $usage.length > 0}
 		<a
 			href="/usage"
 			class="button w-48 h-48 bg-gray-200 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 flex flex-col rounded-xl justify-evenly"
@@ -32,7 +32,7 @@
 			<div class="flex flex-row justify-center dark:text-white mb-6">usage</div>
 		</a>
 	{/if}
-	{#if $computation.length > 1}
+	{#if $computation && $computation.length > 1}
 		<a
 			href="/computation"
 			class="button w-48 h-48 bg-gray-200 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 flex flex-col rounded-xl justify-evenly"
