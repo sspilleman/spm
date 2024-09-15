695c8bcd8d64962d6ec55cd5a84fe75a5aec80f9
Sander Spilleman <sander@spilleman.nl>
2024-09-15 11:04:14 +0200

    2024-09-15T11-04-14

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,14 @@
 import adapter from 'svelte-adapter-deno';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('svelte-adapter-deno').AdapterOptions} */
+const adapterOptions = {
+	out: 'build',
+	precompress: false,
+	envPrefix: '',
+	deps: './deps.ts' // (relative to adapter-deno package)
+};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -13,7 +21,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(adapterOptions)
 	}
 };
 
